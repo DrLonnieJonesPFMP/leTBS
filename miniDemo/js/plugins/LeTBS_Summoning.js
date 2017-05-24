a228ce0731fdc7f885d079c29e752dce78bf46ce
Lecode <lecodeworksgame@gmail.com>
2017-05-24 09:19:52 +0000

    Update demo

--- miniDemo/js/plugins/LeTBS_Summoning.js
+++ miniDemo/js/plugins/LeTBS_Summoning.js
@@ -110,7 +110,7 @@ BattleManagerTBS.updateTurnOrderForSummon = function (caster, summon, type) {
         }
     }
     setTimeout(this._turnOrderVisual.updateOnSummon.bind(this._turnOrderVisual,
-        this._turnOrder, this._activeIndex), 300);
+        this._turnOrder, this._activeIndex), 1000);
 };
 
 BattleManagerTBS.onActiveSummonDeath = function (entity) {
